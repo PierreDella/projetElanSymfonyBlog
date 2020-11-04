38b669a0881d8504933e9ecce3e1339ffb91b22c
PierreDella <66475426+PierreDella@users.noreply.github.com>
2020-11-04 18:29:41 +0100

    update
    Mise en place PP

--- Blog_Recette/src/Controller/RegistrationController.php
+++ Blog_Recette/src/Controller/RegistrationController.php
@@ -9,20 +9,53 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, SluggerInterface $slugger): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
+             //On recupere les images 
+             /** @var UploadedFile $picture */
+            $picture = $form->get('picture')->getData();
+            
+
+            if($picture){
+                $originalPicture = pathinfo($picture->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safePicture = $slugger->slug($originalPicture);
+                $newPicture = $safePicture.'-'.uniqid().'.'.$picture->guessExtension();
+
+                try {
+                    $picture->move(
+                        $this->getParameter('pictures_directory'),
+                        $newPicture
+                    );
+                } catch (FileException $e){
+                    //Mise en place des exceptions a faire plus tard
+                }
+                
+                $user->setPicture($newPicture);
+            }
+            // //On genere un nouveau nom de fichier
+            // $fichier = md5(uniqid()). '.' .$pictures->guessExtensions();
+            
+            // //On copie le fichier dans le dossier uploads
+            // $picture->move(
+            //     $this->getParameter('pictures_directory'),
+            //     $fichier
+            
             // encode the plain password
             $user->setPassword(
                 $passwordEncoder->encodePassword(
@@ -31,15 +64,6 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            /** @var UploadedFile $picture */
-            $picture = $form->get('picture')->getData();
-            // $file = $form['picture']->getData();
-            // $file->move();
-            // if ($picture) {
-            //     $originalPicture = pathinfo($picture->getPseudo(), PATHINFO_FILENAME);
-
-                
-                // }
             $user->setPicture($picture);
 
             $entityManager = $this->getDoctrine()->getManager();
